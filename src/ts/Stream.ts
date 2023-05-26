931e548f4a6dd1627a1a890629e3f0d248a1bcba
kashipu <graficowm@gmail.com>
2023-05-25 19:40:38 -0500

    Clases completadas subido enunciado

---
+++ src/ts/Stream.ts
@@ -0,0 +1,11 @@
+class Stream {
+    id: number;
+    category: string[];
+    temas: string[];
+
+    constructor(id: number, category: string[], temas: string[]) {
+        this.id = id;
+        this.category = category;
+        this.temas = temas;
+    }
+}
